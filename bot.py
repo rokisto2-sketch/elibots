efc131cbebf090b629ab1984ed20dfff9024f4ee
rokisto2-sketch <67227983+rokisto2-sketch@users.noreply.github.com>
2020-06-21 16:54:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,5 +53,5 @@ class MyClient(discord.Client):
 
 # RUN
 client = MyClient()
-token = 'NzE1NTcwMzUwMDYyMTc0MjY5.Xu9UTQ.rB1tSqXJtP-c4FHksX-TdhONDX4'
-client.run(token)
+token = os.environ.get('Token_Bot')
+client.run(str(token))
